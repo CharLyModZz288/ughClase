401002b09f0501f3b0f6011c1771e4eb7a90e908
alumno1 <caesse280801@gmail.com>
2023-12-21 11:26:18 +0100

    Creamos y modificamos EmberPlayer2.dart

--- lib/players/EmberPlayer2.dart
+++ lib/players/EmberPlayer2.dart
@@ -1,47 +1,24 @@
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flame_forge2d/body_component.dart';
-import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ugh2/games/UghGame.dart';
 
-import '../games/UghGame2.dart';
-
-
-class EmberPlayer2Body extends BodyComponent{
-
-  EmberPlayer2Body({Vector2? initialPosition})
-      : super(
-    fixtureDefs: [
-      FixtureDef(
-        CircleShape()..radius = 0.1,
-        restitution: 0.8,
-        friction: 0.4,
-      ),
-    ],
-    bodyDef: BodyDef(
-      //gravityOverride: Vector2(0,-9.8),
-      angularDamping: 0.8,
-      position: initialPosition ?? Vector2.zero(),
-      type: BodyType.dynamic,
-    ),
-  );
-
-  @override
-  void onTapDown(_) {
-    body.applyLinearImpulse(Vector2.random() * 5000);
-  }
-
-  @override
-  Future<void> onLoad() {
-    // TODO: implement onLoad
-    add(EmberPlayer2(position: Vector2(0,0)));
-    return super.onLoad();
-  }
-}
+import '../elementos/Estrella.dart';
+import '../elementos/Gota.dart';
 
 class EmberPlayer2 extends SpriteAnimationComponent
-    with HasGameRef<UghGame2>,KeyboardHandler {
+    with HasGameRef<UghGame>,KeyboardHandler {
+  static const  int I_PLAYER_SUBZERO=0;
+  static const  int I_PLAYER_SCORPIO=1;
+  static const  int I_PLAYER_TANYA=2;
+
+  late int iTipo=-1;
+
+  final _collisionStartColor = Colors.black87;
+  final _defaultColor = Colors.red;
+  late ShapeHitbox hitbox;
 
   int horizontalDirection = 0;
   int verticalDirection = 0;
@@ -51,8 +28,8 @@ class EmberPlayer2 extends SpriteAnimationComponent
   final double aceleracion = 200;
 
   EmberPlayer2({
-    required super.position,
-  }) : super(size: Vector2(100,160), anchor: Anchor.center);
+    required super.position,required super.size,
+  }) : super(anchor: Anchor.center);
 
   @override
   void onLoad() {
@@ -65,6 +42,14 @@ class EmberPlayer2 extends SpriteAnimationComponent
         stepTime: 0.12,
       ),
     );
+    final defaultPaint = Paint()
+      ..color = _defaultColor
+      ..style = PaintingStyle.stroke;
+
+    hitbox = RectangleHitbox();
+    hitbox.paint=defaultPaint;
+    hitbox.isSolid=true;
+    add(hitbox);
   }
 
   @override
@@ -87,13 +72,41 @@ class EmberPlayer2 extends SpriteAnimationComponent
     else if(keysPressed.contains(LogicalKeyboardKey.numpad2)){
       verticalDirection=1;
     }
+
     else{
       horizontalDirection=0;
     }
 
     return true;
   }
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    // TODO: implement onCollision
+    if(other is Gota){
+      this.removeFromParent();
+    }
+    else if(other is Estrella){
+      other.removeFromParent();
+    }
+    //super.onCollision(intersectionPoints, other);
+  }
 
+  @override
+  void onCollisionStart(
+      Set<Vector2> intersectionPoints,
+      PositionComponent other,
+      ) {
+    //super.onCollisionStart(intersectionPoints, other);
+    hitbox.paint.color = _collisionStartColor;
+  }
+
+  @override
+  void onCollisionEnd(PositionComponent other) {
+    /*super.onCollisionEnd(other);
+    if (!isColliding) {
+      hitbox.paint.color = _defaultColor;
+    }*/
+  }
 
   @override
   void update(double dt) {
